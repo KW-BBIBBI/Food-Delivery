fcfa8ff41dcedd620fa371d247bf65a1c125a3a0
ParkEunTaek <142612336+SaltnLight-pet@users.noreply.github.com>
2023-08-31 16:10:15 +0900

    Delete local_kw/devel directory

--- local_kw/devel/share/navfn/cmake/navfn-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(navfn_MESSAGE_FILES "")
-set(navfn_SERVICE_FILES "/home/pet/Food-Delivery/local_kw/src/navigation/navfn/srv/MakeNavPlan.srv;/home/pet/Food-Delivery/local_kw/src/navigation/navfn/srv/SetCostmap.srv")
